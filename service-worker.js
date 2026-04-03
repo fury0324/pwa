d1a06b8d51290acc457be6c01ec9bea2db0c81f8
fury0324 <antoniosteven664@gmail.com>
2026-04-04 04:36:09 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,15 @@
-const CACHE_NAME = 'myapp-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './node_modules/bootstrap/dist/css/bootstrap.min.css',
-  './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+  './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/apple-touch-icon.png',
   './icons/favicon-32x32.png',
-  './icons/favicon-16x16.png'
+  './icons/favicon-16x16.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js',
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css'
 ];
 
 self.addEventListener('install', event => {
