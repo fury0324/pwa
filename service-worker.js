8c62992aeb4deb1e595d7ca3feedacea38615822
fury0324 <antoniosteven664@gmail.com>
2026-04-04 04:56:05 +0800

    Update cached URLs in service worker

--- service-worker.js
+++ service-worker.js
@@ -7,6 +7,7 @@ const urlsToCache = [
   './apple-touch-icon.png',
   './favicon-32x32.png',
   './favicon-16x16.png',
+  './bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js',
   'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css'
